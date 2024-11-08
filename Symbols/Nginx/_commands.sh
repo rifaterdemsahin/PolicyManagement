0768b68c2e8ca41265ebbcaa5d7fdc7ab257a2fe
Erdem <rifaterdemsahin@gmail.com>
2024-11-08 10:33:49 +0000

    nginx push

--- Symbols/Nginx/_commands.sh
+++ Symbols/Nginx/_commands.sh
@@ -5,4 +5,17 @@ kubectl apply -f frontend-deployment.yaml
 kubectl apply -f nginx-config.yaml
 
 kubectl get pods -l app=frontend
-kubectl exec -it <frontend-pod-name> -- curl http://backend-service:8080
+
+@rifaterdemsahin ➜ /workspaces/PolicyManagement/Symbols/Nginx (main) $ kubectl apply -f backend-deployment.yaml
+deployment.apps/backend-deployment created
+@rifaterdemsahin ➜ /workspaces/PolicyManagement/Symbols/Nginx (main) $ kubectl apply -f backend-service.yaml
+service/backend-service created
+@rifaterdemsahin ➜ /workspaces/PolicyManagement/Symbols/Nginx (main) $ kubectl apply -f frontend-deployment.yaml
+deployment.apps/frontend-deployment created
+@rifaterdemsahin ➜ /workspaces/PolicyManagement/Symbols/Nginx (main) $ kubectl apply -f nginx-config.yaml
+configmap/nginx-config created
+@rifaterdemsahin ➜ /workspaces/PolicyManagement/Symbols/Nginx (main) $ kubectl get pods -l app=frontend
+NAME                                  READY   STATUS              RESTARTS   AGE
+frontend-deployment-b4f657fc4-r5sm8   0/1     ContainerCreating   0          15s
+
+kubectl exec -it frontend-deployment-b4f657fc4-r5sm8 -- curl http://backend-service:8080
