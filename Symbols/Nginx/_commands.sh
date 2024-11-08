06a808a773ac8f5737a73a43254fd8fedc68bb0c
Erdem <rifaterdemsahin@gmail.com>
2024-11-08 10:36:35 +0000

    nginx push: happiness

--- Symbols/Nginx/_commands.sh
+++ Symbols/Nginx/_commands.sh
@@ -19,3 +19,17 @@ NAME                                  READY   STATUS              RESTARTS   AGE
 frontend-deployment-b4f657fc4-r5sm8   0/1     ContainerCreating   0          15s
 
 kubectl exec -it frontend-deployment-b4f657fc4-r5sm8 -- curl http://backend-service:8080
+First image pullback 
+===============================================================================================
+
+deployment.apps/backend-deployment created
+@rifaterdemsahin ➜ /workspaces/PolicyManagement/Symbols/Nginx (main) $ kubectl apply -f backend-service.yaml
+service/backend-service created
+@rifaterdemsahin ➜ /workspaces/PolicyManagement/Symbols/Nginx (main) $ kubectl apply -f frontend-deployment.yaml
+deployment.apps/frontend-deployment created
+@rifaterdemsahin ➜ /workspaces/PolicyManagement/Symbols/Nginx (main) $ kubectl apply -f nginx-config.yaml
+configmap/nginx-config created
+@rifaterdemsahin ➜ /workspaces/PolicyManagement/Symbols/Nginx (main) $ kubectl get pods -l app=frontend
+NAME                                   READY   STATUS    RESTARTS   AGE
+frontend-deployment-58868f5d95-8grbb   1/1     Running   0          13s
+@rifaterdemsahin ➜ /workspaces/PolicyManagement/Symbols/Nginx (main) $ 
